3bbe566d4ffd41ebcd5878c050fbc8139f24e388
Ypuello <Yensi.Puello@theksquaregroup.com>
2022-07-17 09:21:56 -0400

    ...

--- build/push_changes.sh
+++ build/push_changes.sh
@@ -1,7 +1,7 @@
 # Push changes
 
 git remote -v
-git remote remove
+git remote remove https://github.com/Ypuello/packagebased-demo.git
 git remote add https://github.com/Ypuello/packagebased-demo.git
 git checkout feature-branch
 git push https://github.com/Ypuello/packagebased-demo.git feature-branch:master
