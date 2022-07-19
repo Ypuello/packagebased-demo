75157333754d32d7cbc1490444294ef3b0ece83f
Ypuello <Yensi.Puello@theksquaregroup.com>
2022-07-19 11:15:33 -0400

    ...

--- build/push_changes.sh
+++ build/push_changes.sh
@@ -1,4 +1,2 @@
 # Push changes
-
-git commit -m "initial commit"
-git push https://github.com/Ypuello/packagebased-demo.git master
+git push https://github.com/Ypuello/packagebased-demo.git feature-branch:master
